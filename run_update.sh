35fde3eb12e26d022ec20e4e38dad4dc6c835fe5
Christian Dähn <c.daehn@techdivision.com>
2022-02-08 12:47:13 +0100

    update script

---
+++ run_update.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+source .env/bin/activate
+python update.py src
+deactivate
+
+git --no-pager diff
+echo "What have you changed?"
+read -r message
+git add src/*.gs
+git add src/*.html
+git commit -a -m"$message"
+git push origin
+git push td_origin
